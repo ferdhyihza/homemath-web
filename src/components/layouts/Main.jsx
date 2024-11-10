d297f07f13b8eff8bf1b6126c4087e21435ae5a2
Ferdhy Ihza Armanda <ferdhyia.dev@gmail.com>
2024-11-10 13:13:07 +0700

    fix re-render quotes

--- src/components/layouts/Main.jsx
+++ src/components/layouts/Main.jsx
@@ -2,10 +2,15 @@ import Navbar from './Navbar';
 import { useLocation } from 'react-router-dom';
 import Quotes from './Quotes';
 import ScrollToTop from '../ScrollToTop';
+import { useMemo } from 'react';
 
 export default function Main(props) {
   const location = useLocation();
   const isMateriPage = location.pathname.includes('/materi/');
+
+  const memoizedQuotes = useMemo(() => {
+    return isMateriPage ? <Quotes /> : null;
+  }, [isMateriPage]);
 
   return (
     <>
@@ -18,7 +23,7 @@ export default function Main(props) {
       <div className="main container">
         {/* eslint-disable-next-line react/prop-types */}
         <div className="pt-4">{props.children}</div>
-        {isMateriPage && <Quotes />}
+        {memoizedQuotes}
       </div>
     </>
   );
