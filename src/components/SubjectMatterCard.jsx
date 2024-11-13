799a0b7fbce5c2b2262b13c4389e2c572cbe35e6
Ferdhy Ihza Armanda <ferdhyia.dev@gmail.com>
2024-11-13 11:44:00 +0700

    progres materi

--- src/components/SubjectMatterCard.jsx
+++ src/components/SubjectMatterCard.jsx
@@ -7,7 +7,14 @@ export default function SubjectMatterCard(props) {
       <Link to={props.link} className={`${props.className} border d-flex rounded-3 mb-4 p-2 materi`}>
         {props.number && <div className="number p-3 fs-1">{props.number}</div>}
         <div className="content p-2 w-100">
-          <h5 className="fw-bold p-2 d-inline-block hover-underline">{props.title}</h5>
+          <div className="d-flex justify-content-between">
+            <h5 className="fw-bold p-2 d-inline-block hover-underline">{props.title}</h5>
+            {props.progress && (
+              <h5 className="fw-bold p-2 d-inline-block text-lightblue-subtle">
+                <span className="fw-medium">Progress</span>: {props.progress}%
+              </h5>
+            )}
+          </div>
           <hr className="my-0" />
           <ul className="p-1 mb-0 d-flex gap-3">{props.children}</ul>
         </div>
--- src/pages/SubjectMatter.jsx
+++ src/pages/SubjectMatter.jsx
@@ -7,115 +7,86 @@ import { useEffect, useState } from 'react';
 export default function SubjectMatter() {
   const [isPengantarSPLTVDone, setIsPengantarSPLTVDone] = useState(false);
   const [isUnsurPadaSPLTVDone, setIsUnsurPadaSPLTVDone] = useState(false);
-  const [isBentukUmumSPLTVDone, setIsBentukUmumSPLTVDone] = useState(false);
+  const [, setIsBentukUmumSPLTVDone] = useState(false);
   const [isBentukUmumSPLTV2Done, setIsBentukUmumSPLTV2Done] = useState(false);
+  const [progressPengantarSPLTV, setProgressPengantarSPLTV] = useState('0');
 
   const [isMetodePenyelesaianSPLTVDone, setIsMetodePenyelesaianSPLTVDone] = useState(false);
   const [isMetodePenyelesaianEliminasiDone, setIsMetodePenyelesaianEliminasiDone] = useState(false);
   const [isMetodePenyelesaianSubstitusiDone, setIsMetodePenyelesaianSubstitusiDone] = useState(false);
   const [isMetodePenyelesaianCampuranDone, setIsMetodePenyelesaianCampuranDone] = useState(false);
+  const [progressMetodePenyelesaianSPLTV, setProgressMetodePenyelesaianSPLTV] = useState('0');
 
   const [, setIsPenggunaanSPLTVDone] = useState(false);
   const [isPenggunaanSPLTV2Done, setIsPenggunaanSPLTV2Done] = useState(false);
+  const [progressPenggunaanSPLTV, setProgressPenggunaanSPLTV] = useState('0');
 
   useEffect(() => {
     const sessionData = JSON.parse(localStorage.getItem('sessionData'));
-    if (sessionData?.PengantarSPLTV) setIsPengantarSPLTVDone(true);
-    if (sessionData?.UnsurPadaSPLTV) setIsUnsurPadaSPLTVDone(true);
-    if (sessionData?.BentukUmumSPLTV) setIsBentukUmumSPLTVDone(true);
-    if (sessionData?.BentukUmumSPLTV2) setIsBentukUmumSPLTV2Done(true);
+    if (sessionData?.PengantarSPLTV) {
+      setIsPengantarSPLTVDone(true);
+      setProgressPengantarSPLTV(25);
+    }
+    if (sessionData?.UnsurPadaSPLTV) {
+      setIsUnsurPadaSPLTVDone(true);
+      setProgressPengantarSPLTV(50);
+    }
+    if (sessionData?.BentukUmumSPLTV) {
+      setIsBentukUmumSPLTVDone(true);
+      setProgressPengantarSPLTV(75);
+    }
+    if (sessionData?.BentukUmumSPLTV2) {
+      setIsBentukUmumSPLTV2Done(true);
+      setProgressPengantarSPLTV(100);
+    }
 
-    if (sessionData?.MetodePenyelesaianSPLTV) setIsMetodePenyelesaianSPLTVDone(true);
-    if (sessionData?.MetodePenyelesaianEliminasi) setIsMetodePenyelesaianEliminasiDone(true);
-    if (sessionData?.MetodePenyelesaianSubstitusi) setIsMetodePenyelesaianSubstitusiDone(true);
-    if (sessionData?.MetodePenyelesaianCampuran) setIsMetodePenyelesaianCampuranDone(true);
+    if (sessionData?.MetodePenyelesaianSPLTV) {
+      setIsMetodePenyelesaianSPLTVDone(true);
+      setProgressMetodePenyelesaianSPLTV(25);
+    }
+    if (sessionData?.MetodePenyelesaianEliminasi) {
+      setIsMetodePenyelesaianEliminasiDone(true);
+      setProgressMetodePenyelesaianSPLTV(50);
+    }
+    if (sessionData?.MetodePenyelesaianSubstitusi) {
+      setIsMetodePenyelesaianSubstitusiDone(true);
+      setProgressMetodePenyelesaianSPLTV(75);
+    }
+    if (sessionData?.MetodePenyelesaianCampuran) {
+      setIsMetodePenyelesaianCampuranDone(true);
+      setProgressMetodePenyelesaianSPLTV(100);
+    }
 
-    if (sessionData?.PenggunaanSPLTV) setIsPenggunaanSPLTVDone(true);
-    if (sessionData?.PenggunaanSPLTV2) setIsPenggunaanSPLTV2Done(true);
+    if (sessionData?.PenggunaanSPLTV) {
+      setIsPenggunaanSPLTVDone(true);
+      setProgressPenggunaanSPLTV(50);
+    }
+    if (sessionData?.PenggunaanSPLTV2) {
+      setIsPenggunaanSPLTV2Done(true);
+      setProgressPenggunaanSPLTV(100);
+    }
   }, []);
 
   return (
     <Main>
       <h3 className="fw-bold pb-3">Materi</h3>
-      <SubjectMatterCard number="01" title="Pengantar SPLTV" link="pengantar-spltv">
+      <SubjectMatterCard number="01" title="Pengantar SPLTV" link="pengantar-spltv" progress={progressPengantarSPLTV}>
         <li>
           <Link to="pengantar-spltv">Pengantar SPLTV </Link>
-          {isPengantarSPLTVDone ? '✅' : '🔃'}
         </li>
+        <li>{isPengantarSPLTVDone ? <Link to="unsur-pada-spltv">Unsur pada SPLTV</Link> : <span className="text-secondary fw-light">Unsur pada SPLTV</span>}</li>
+        <li>{isUnsurPadaSPLTVDone ? <Link to="bentuk-umum-spltv">Bentuk Umum SPLTV </Link> : <span className="text-secondary fw-light">Bentuk Umum SPLTV</span>}</li>
+      </SubjectMatterCard>
+      <SubjectMatterCard number="02" title="Metode Penyelesaian SPLTV" link="metode-penyelesaian-spltv" progress={progressMetodePenyelesaianSPLTV} className={isBentukUmumSPLTV2Done ? '' : 'disabled text-secondary'}>
+        <li>{isBentukUmumSPLTV2Done ? <Link to="metode-penyelesaian-spltv">Macam-macam Metode Penyelesaian SPLTV</Link> : <span className="text-secondary fw-light">Macam-macam Metode Penyelesaian SPLTV</span>}</li>
+        <li>{isMetodePenyelesaianSPLTVDone ? <Link to="metode-penyelesaian-spltv/eliminasi">Metode Eliminasi</Link> : <span className="text-secondary fw-light">Metode Eliminasi</span>}</li>
+        <li>{isMetodePenyelesaianEliminasiDone ? <Link to="metode-penyelesaian-spltv/substitusi">Metode Substitusi</Link> : <span className="text-secondary fw-light">Metode Substitusi</span>}</li>
         <li>
-          {isPengantarSPLTVDone ? (
-            <>
-              <Link to="unsur-pada-spltv">Unsur pada SPLTV</Link>
-              {isUnsurPadaSPLTVDone ? '✅' : isPengantarSPLTVDone ? '🔃' : ''}
-            </>
-          ) : (
-            <span className="text-secondary">Unsur pada SPLTV</span>
-          )}
-        </li>
-        <li>
-          {isUnsurPadaSPLTVDone ? (
-            <>
-              <Link to="bentuk-umum-spltv">Bentuk Umum SPLTV </Link>
-              {isBentukUmumSPLTV2Done ? '✅' : isUnsurPadaSPLTVDone ? '🔃' : ''}
-            </>
-          ) : (
-            <span className="text-secondary">Bentuk Umum SPLTV</span>
-          )}
+          {isMetodePenyelesaianSubstitusiDone ? <Link to="metode-penyelesaian-spltv/campuran">Metode Campuran (Eliminasi & Substitusi)</Link> : <span className="text-secondary fw-light">Metode Campuran (Eliminasi & Substitusi)</span>}
         </li>
       </SubjectMatterCard>
-      <SubjectMatterCard number="02" title="Metode Penyelesaian SPLTV" link="metode-penyelesaian-spltv" className={isBentukUmumSPLTV2Done ? '' : 'disabled text-secondary'}>
-        <li>
-          {isBentukUmumSPLTVDone && isBentukUmumSPLTV2Done ? (
-            <>
-              <Link to="metode-penyelesaian-spltv">Macam-macam Metode Penyelesaian SPLTV</Link>
-              {isMetodePenyelesaianSPLTVDone ? '✅' : isBentukUmumSPLTV2Done ? '🔃' : ''}
-            </>
-          ) : (
-            <span className="text-secondary">Macam-macam Metode Penyelesaian SPLTV</span>
-          )}
-        </li>
-        <li>
-          {isMetodePenyelesaianSPLTVDone ? (
-            <>
-              <Link to="metode-penyelesaian-spltv/eliminasi">Metode Eliminasi</Link>
-              {isMetodePenyelesaianEliminasiDone ? '✅' : isMetodePenyelesaianSPLTVDone ? '🔃' : ''}
-            </>
-          ) : (
-            <span className="text-secondary">Metode Eliminasi</span>
-          )}
-        </li>
-        <li>
-          {isMetodePenyelesaianEliminasiDone ? (
-            <>
-              <Link to="metode-penyelesaian-spltv/substitusi">Metode Substitusi</Link>
-              {isMetodePenyelesaianSubstitusiDone ? '✅' : isMetodePenyelesaianEliminasiDone ? '🔃' : ''}
-            </>
-          ) : (
-            <span className="text-secondary">Metode Substitusi</span>
-          )}
-        </li>
-        <li>
-          {isMetodePenyelesaianSubstitusiDone ? (
-            <>
-              <Link to="metode-penyelesaian-spltv/campuran">Metode Campuran (Eliminasi & Substitusi)</Link>
-              {isMetodePenyelesaianCampuranDone ? '✅' : isMetodePenyelesaianSubstitusiDone ? '🔃' : ''}
-            </>
-          ) : (
-            <span className="text-secondary">Metode Campuran (Eliminasi & Substitusi)</span>
-          )}
-        </li>
-      </SubjectMatterCard>
-      <SubjectMatterCard number="03" title="Penggunaan SPLTV pada Kehidupan Sehari-hari" link="penggunaan-spltv" className={isMetodePenyelesaianCampuranDone ? '' : 'disabled text-secondary'}>
-        <li>
-          {isMetodePenyelesaianCampuranDone ? (
-            <>
-              <Link to="penggunaan-spltv">Untuk apa kita mempelajari SPLTV?</Link>
-              {isPenggunaanSPLTV2Done ? '✅' : isMetodePenyelesaianCampuranDone ? '🔃' : ''}
-            </>
-          ) : (
-            <span className="text-secondary">Untuk apa kita mempelajari SPLTV?</span>
-          )}
-        </li>
+      <SubjectMatterCard number="03" title="Penggunaan SPLTV pada Kehidupan Sehari-hari" link="penggunaan-spltv" progress={progressPenggunaanSPLTV} className={isMetodePenyelesaianCampuranDone ? '' : 'disabled text-secondary'}>
+        <li>{isMetodePenyelesaianCampuranDone ? <Link to="penggunaan-spltv">Untuk apa kita mempelajari SPLTV?</Link> : <span className="text-secondary fw-light">Untuk apa kita mempelajari SPLTV?</span>}</li>
       </SubjectMatterCard>
       <h3 className="fw-bold py-3">Lainnya</h3>
       <div className="row">
